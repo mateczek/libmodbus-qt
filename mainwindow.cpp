c6afd004312b909683f99055c161a4569704a148
mateczek <marcof@interia.pl>
2023-04-17 23:04:16 +0200

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,8 +7,15 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     mb=new modbusEngine(reg,sizeof(reg),0,"127.0.0.1",this);
+        // stworzenie obiektu klasy modbusEngine i przekazanie takich parametrów jak
+        //wskaźnik na tablice rejestrów
+        //rozmiar rejestrów do odczytania
+        //bazowy rejestr  tutaj 0
+        //adres ip serwera modbus
+        
     connect(mb,SIGNAL(dataRaded()),this,SLOT(dataRead()));
-    mb->modbusStart();
+        //gdy nowe dane przybędą wywołaj slot dataRead()
+    mb->modbusStart(); //połącz
 }
 
 MainWindow::~MainWindow()
@@ -20,10 +27,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    mb->writeBit(1,1);
+    mb->writeBit(1,1); //przykładowa funkcja zapisz bit - ustawia jedynkę
 }
 
-void MainWindow::dataRead()
+void MainWindow::dataRead()  //aktualizacja cyklicznych danych na formatce
 {
     ui->lineEdit->setText(QString::number(reg[0]));
     ui->lineEdit_2->setText(QString::number(reg[1]));
